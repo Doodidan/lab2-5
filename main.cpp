64482d84f41c7ef507308e820ea9126956681e54
Георгий <@172.19.104.188>
2014-12-12 19:02:05 +0400

    Создано

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Георгий
+ *
+ * Created on 12 Декабрь 2014 г., 19:00
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
